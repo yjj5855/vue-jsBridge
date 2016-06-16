a87f2f8c2133933c0c1bc2691cc7f555a01dd07c
杨佳军 <yangjj@chezhil.com>
2016-06-16 10:09:17 +0800

    no message

--- device.js
+++ device.js
@@ -1,63 +1,62 @@
+"use strict";
+var device = {};
+var ua = navigator.userAgent;
 
-    "use strict";
-    var device = {};
-    var ua = navigator.userAgent;
+var android = ua.match(/(Android);?[\s\/]+([\d.]+)?/);
+var ipad = ua.match(/(iPad).*OS\s([\d_]+)/);
+var ipod = ua.match(/(iPod)(.*OS\s([\d_]+))?/);
+var iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/);
 
-    var android = ua.match(/(Android);?[\s\/]+([\d.]+)?/);
-    var ipad = ua.match(/(iPad).*OS\s([\d_]+)/);
-    var ipod = ua.match(/(iPod)(.*OS\s([\d_]+))?/);
-    var iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/);
+device.ios = device.android = device.iphone = device.ipad = device.androidChrome = false;
 
-    device.ios = device.android = device.iphone = device.ipad = device.androidChrome = false;
+// Android
+if (android) {
+    device.os = 'android';
+    device.osVersion = android[2];
+    device.android = true;
+    device.androidChrome = ua.toLowerCase().indexOf('chrome') >= 0;
+}
+if (ipad || iphone || ipod) {
+    device.os = 'ios';
+    device.ios = true;
+}
+// iOS
+if (iphone && !ipod) {
+    device.osVersion = iphone[2].replace(/_/g, '.');
+    device.iphone = true;
+}
+if (ipad) {
+    device.osVersion = ipad[2].replace(/_/g, '.');
+    device.ipad = true;
+}
+if (ipod) {
+    device.osVersion = ipod[3] ? ipod[3].replace(/_/g, '.') : null;
+    device.iphone = true;
+}
+// iOS 8+ changed UA
+if (device.ios && device.osVersion && ua.indexOf('Version/') >= 0) {
+    if (device.osVersion.split('.')[0] === '10') {
+        device.osVersion = ua.toLowerCase().split('version/')[1].split(' ')[0];
+    }
+}
 
-    // Android
-    if (android) {
-        device.os = 'android';
-        device.osVersion = android[2];
-        device.android = true;
-        device.androidChrome = ua.toLowerCase().indexOf('chrome') >= 0;
-    }
-    if (ipad || iphone || ipod) {
-        device.os = 'ios';
-        device.ios = true;
-    }
-    // iOS
-    if (iphone && !ipod) {
-        device.osVersion = iphone[2].replace(/_/g, '.');
-        device.iphone = true;
-    }
-    if (ipad) {
-        device.osVersion = ipad[2].replace(/_/g, '.');
-        device.ipad = true;
-    }
-    if (ipod) {
-        device.osVersion = ipod[3] ? ipod[3].replace(/_/g, '.') : null;
-        device.iphone = true;
-    }
-    // iOS 8+ changed UA
-    if (device.ios && device.osVersion && ua.indexOf('Version/') >= 0) {
-        if (device.osVersion.split('.')[0] === '10') {
-            device.osVersion = ua.toLowerCase().split('version/')[1].split(' ')[0];
+// Webview
+device.webView = (iphone || ipad || ipod) && ua.match(/.*AppleWebKit(?!.*Safari)/i);
+
+
+// OS classes
+if (device.os) {
+    classNames.push(device.os, device.os + '-' + device.osVersion.split('.')[0], device.os + '-' + device.osVersion.replace(/\./g, '-'));
+    if (device.os === 'ios') {
+        var major = parseInt(device.osVersion.split('.')[0], 10);
+        for (var i = major - 1; i >= 6; i--) {
+            classNames.push('ios-gt-' + i);
         }
     }
 
-    // Webview
-    device.webView = (iphone || ipad || ipod) && ua.match(/.*AppleWebKit(?!.*Safari)/i);
+}
 
+// keng..
+device.isWeixin = /MicroMessenger/i.test(ua);
 
-    // OS classes
-    if (device.os) {
-        classNames.push(device.os, device.os + '-' + device.osVersion.split('.')[0], device.os + '-' + device.osVersion.replace(/\./g, '-'));
-        if (device.os === 'ios') {
-            var major = parseInt(device.osVersion.split('.')[0], 10);
-            for (var i = major - 1; i >= 6; i--) {
-                classNames.push('ios-gt-' + i);
-            }
-        }
-
-    }
-
-    // keng..
-    device.isWeixin = /MicroMessenger/i.test(ua);
-
-    export default device;
+export default device;
